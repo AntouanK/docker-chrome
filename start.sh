4560088a6bcf01bab00a3a14ba546188ffe91249
Antonis Karamitros @antouank <ablackjim228@gmail.com>
2015-09-28 22:30:24 +0100

    [1.0.1] - accept many arguments on the google-chrome command

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ Xvfb :1 -screen 0 1280x720x16 &
 
 sleep 1;
 
-echo "\n[start script] Starting Google Chrome at $1";
+echo "\n[start script] Starting Google Chrome at $@";
 google-chrome \
 --no-sandbox \
 --user-data-dir=/home/chrome/ \
 --no-first-run \
 --remote-debugging-port=9222 \
-$1 \
+$@ \
 &
 
 sleep 1;
